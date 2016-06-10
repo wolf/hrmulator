1feca59e427e13fdb8052266c5abaabc5938c9c4
Wolf <wolf@zv.cx>
2016-06-10 07:21:47 -0400

    rearrange instructions to match game

--- Instructions.py
+++ Instructions.py
@@ -8,15 +8,15 @@ accumulator and the program_counter.  There are 12 concrete instructions:
     NoOp                    no_op
     MoveFromInbox           move_from_inbox
     MoveToOutbox            move_to_outbox
-    CopyTo                  copy_to
     CopyFrom                copy_from
+    CopyTo                  copy_to
     Add                     add
     Subtract                subtract
+    BumpUp                  bump_up
+    BumpDown                bump_down
     Jump                    jump_to
     JumpIfZero              jump_if_zero_to
     JumpIfNegative          jump_if_negative_to
-    BumpUp                  bump_up
-    BumpDown                bump_down
 
 In each case the class variable `token` is used both for printing and for the
 symbol lookup needed in the assembler.  The `execute` function does the main
@@ -87,22 +87,22 @@ class AbstractTileInstruction(AbstractInstruction):
         self.tile_index = tile_index
 
 
-class CopyTo(AbstractTileInstruction):
-    token = "copy_to"
-
-    def execute(self, computer):
-        computer.assertAccumulatorIsNotEmpty()
-        computer.memory[self.tile_index] = computer.accumulator
-        computer.program_counter += 1
-        computer.total_steps_executed += 1
-
-
 class CopyFrom(AbstractTileInstruction):
     token = "copy_from"
 
     def execute(self, computer):
         computer.assertMemoryTileIsNotEmpty(self.tile_index)
         computer.accumulator = computer.memory[self.tile_index]
+        computer.program_counter += 1
+        computer.total_steps_executed += 1
+
+
+class CopyTo(AbstractTileInstruction):
+    token = "copy_to"
+
+    def execute(self, computer):
+        computer.assertAccumulatorIsNotEmpty()
+        computer.memory[self.tile_index] = computer.accumulator
         computer.program_counter += 1
         computer.total_steps_executed += 1
 
@@ -123,6 +123,42 @@ class Subtract(AbstractTileInstruction):
     def execute(self, computer):
         computer.assertMemoryTileIsNotEmpty(self.tile_index)
         computer.accumulator -= computer.memory[self.tile_index]
+        computer.program_counter += 1
+        computer.total_steps_executed += 1
+
+
+class BumpUp(AbstractTileInstruction):
+    """
+    Increment the value in a given memory tile by 1, and copy it into the
+    accumulator.
+    """
+    token = "bump_up"
+
+    def __init__(self, tile_index):
+        self.tile_index = tile_index
+
+    def execute(self, computer):
+        computer.assertMemoryTileIsNotEmpty(self.tile_index)
+        computer.memory[self.tile_index] += 1
+        computer.accumulator = computer.memory[self.tile_index]
+        computer.program_counter += 1
+        computer.total_steps_executed += 1
+
+
+class BumpDown(AbstractTileInstruction):
+    """
+    Decrement the value in a given memory tile by 1, and copy it into the
+    accumulator.
+    """
+    token = "bump_down"
+
+    def __init__(self, tile_index):
+        self.tile_index = tile_index
+
+    def execute(self, computer):
+        computer.assertMemoryTileIsNotEmpty(self.tile_index)
+        computer.memory[self.tile_index] -= 1
+        computer.accumulator = computer.memory[self.tile_index]
         computer.program_counter += 1
         computer.total_steps_executed += 1
 
@@ -177,53 +213,17 @@ class JumpIfNegative(Jump):
         computer.total_steps_executed += 1
 
 
-class BumpUp(AbstractTileInstruction):
-    """
-    Increment the value in a given memory tile by 1, and copy it into the
-    accumulator.
-    """
-    token = "bump_up"
-
-    def __init__(self, tile_index):
-        self.tile_index = tile_index
-
-    def execute(self, computer):
-        computer.assertMemoryTileIsNotEmpty(self.tile_index)
-        computer.memory[self.tile_index] += 1
-        computer.accumulator = computer.memory[self.tile_index]
-        computer.program_counter += 1
-        computer.total_steps_executed += 1
-
-
-class BumpDown(AbstractTileInstruction):
-    """
-    Decrement the value in a given memory tile by 1, and copy it into the
-    accumulator.
-    """
-    token = "bump_down"
-
-    def __init__(self, tile_index):
-        self.tile_index = tile_index
-
-    def execute(self, computer):
-        computer.assertMemoryTileIsNotEmpty(self.tile_index)
-        computer.memory[self.tile_index] -= 1
-        computer.accumulator = computer.memory[self.tile_index]
-        computer.program_counter += 1
-        computer.total_steps_executed += 1
-
-
 InstructionCatalog = [
     NoOp,
     MoveFromInbox,
     MoveToOutbox,
-    CopyTo,
     CopyFrom,
+    CopyTo,
     Add,
     Subtract,
+    BumpUp,
+    BumpDown,
     Jump,
     JumpIfZero,
-    JumpIfNegative,
-    BumpUp,
-    BumpDown
+    JumpIfNegative
 ]
