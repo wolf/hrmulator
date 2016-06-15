bf55a68ddb1ec9e7dc0b85334b4daa6338530e0b
Wolf <wolf@zv.cx>
2016-06-15 18:56:23 -0400

    make it installable

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@ TO DO:
 
 - more docstrings
 - tests
-- "job control" --> supply in a program the inbox, the expected outbox, run it - like a test
 - clean up assembler
 - keep symbols around,
   -  use symbolic names for memory locations
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hrmulator',
+      version='0.1',
+      description='a simple computer, programmable in its own assembly language',
+      url='http://github.com/wolf/hrmulator',
+      author='Wolf',
+      author_email='Wolf@zv.cx',
+      license='MIT',
+      packages=['hrmulator'],
+      zip_safe=False)
